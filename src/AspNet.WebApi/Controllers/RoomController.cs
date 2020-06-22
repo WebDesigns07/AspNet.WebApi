ce3232c5c5dd6589c25bbbe963e57b8aec16833a
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-06-22 23:42:30 +0800

    feature
    WebApi增删改查。
    错误处理。

---
+++ src/AspNet.WebApi/Controllers/RoomController.cs
@@ -0,0 +1,69 @@
+﻿using AspNet.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using static AspNet.WebApi.Models.Room;
+
+namespace AspNet.WebApi.Controllers
+{
+    public class RoomController : ApiController
+    {
+        // GET: api/Room
+        public IEnumerable<Room> GetRooms()
+        {
+            return Rooms;
+        }
+
+        // GET: api/Room/5
+        public IHttpActionResult GetRoom(int id)
+        {
+            try
+            {
+                return Ok(Rooms.Where(room => room.RoomId == id).Single());
+            }
+            catch (InvalidOperationException)
+            {
+                return NotFound();
+            }
+        }
+
+        // POST: api/Room
+        public void PostRoom([FromBody]Room newRoom)
+        {
+            Rooms.Add(newRoom);
+        }
+
+        // PUT: api/Room/5
+        public IHttpActionResult PutRoom(int id, [FromBody]Room value)
+        {
+            try
+            {
+                Rooms.Remove(Rooms.Where(room => room.RoomId == id).Single());
+                Rooms.Add(value);
+                return Ok();
+            }
+            catch (InvalidOperationException)
+            {
+                return BadRequest();
+            }
+        }
+
+        // DELETE: api/Room/5
+        public IHttpActionResult DeleteRoom(int id)
+        {
+            try
+            {
+                Rooms.Remove(Rooms.Where(room => room.RoomId == id).Single());
+                return Ok();
+            }
+            catch (InvalidOperationException)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ src/AspNet.WebApi/Models/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNet.WebApi.Models
+{
+    public class Room
+    {
+        public static List<Room> Rooms = new List<Room>
+        {
+            new Room{ RoomId = 1, Area = 57.5, RoomName = "325E"},
+            new Room{ RoomId = 2, Area = 27.5, RoomName = "325F"},
+            new Room{ RoomId = 3, Area = 157.5, RoomName = "325G"},
+        };
+
+        public int RoomId { get; set; }
+        public string RoomName { get; set; }
+        public double Area { get; set; }
+    }
+}
